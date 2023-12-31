ace62a838dd839683730dd39bddd6a1d21cd858d
Chrisanto76 <antonypoure2@gmail.com>
2023-12-31 13:42:16 +0100

    Update SocialLinks styling

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -7,7 +7,7 @@ const SocialLinks = () => {
 	return (
 		<div className="flex flex-col top-[35%] left-0 fixed">
 			<ul>
-				<li className="flex justify-between items-center w-40 h-1/4 px-4 bg-black ">
+				<li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
 					<a
 						href="https://www.linkedin.com/in/antony-poure-874297232/"
 						className="flex justify-between items-center w-full text-white"
