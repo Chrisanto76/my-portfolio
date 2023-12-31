527076339196d8eaee9478c50a22a1c21d23dfd1
Chrisanto76 <antonypoure2@gmail.com>
2023-12-31 16:17:34 +0100

    Fix Navbar and SocialLinks components

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
@@ -49,30 +49,27 @@ const SocialLinks = () => {
 	return (
 		<div className="flex flex-col top-[35%] left-0 fixed">
 			<ul>
-				{links.map((link) => (
+				{links.map(({ id, child, href, style, download }) => (
 					<li
-						key={link.id}
-						className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${link.style}`}
+						key={id}
+						className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500"
 					>
 						<a
-							href={link.href}
-							className="flex justify-between items-center w-full text-white"
-							download={link.download}
+							href={href}
+							className={
+								// eslint-disable-next-line no-useless-concat
+								'flex justify-between items-center w-full text-white' +
+								' ' +
+								style
+							}
+							download={download}
+							target="_blank"
+							rel="noreferrer"
 						>
-							{link.child}
+							{child}
 						</a>
 					</li>
 				))}
-				{/* <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
-					<a
-						href="https://www.linkedin.com/in/antony-poure-874297232/"
-						className="flex justify-between items-center w-full text-white"
-					>
-						<>
-							LinkedIn <FaLinkedin size={30} />
-						</>
-					</a>
-				</li> */}
 			</ul>
 		</div>
 	);
