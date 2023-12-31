5539aa3ec88ca05424f2034d597a4ac71065f241
Chrisanto76 <antonypoure2@gmail.com>
2023-12-31 15:50:46 +0100

    Add new social links and update icons

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
-import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonFill } from 'react-icons/bs';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const SocialLinks = () => {
+	const links = [
+		{
+			id: 1,
+			child: (
+				<>
+					linkedIn <FaLinkedin size={30} />
+				</>
+			),
+			href: 'https://www.linkedin.com/in/antony-poure-874297232/',
+			style: 'rounded-tr-md',
+		},
+		{
+			id: 2,
+			child: (
+				<>
+					Github <FaGithub size={30} />
+				</>
+			),
+			href: 'https://github.com/Chrisanto76',
+		},
+		{
+			id: 3,
+			child: (
+				<>
+					Mail <HiOutlineMail size={30} />
+				</>
+			),
+			href: 'mailto:antonypoure2@gmail.com',
+		},
+		{
+			id: 4,
+			child: (
+				<>
+					Resume <BsFillPersonLinesFill size={30} />
+				</>
+			),
+			href: './resume.pdf',
+			style: 'rounded-br-md',
+			download: true,
+		},
+	];
+
 	return (
 		<div className="flex flex-col top-[35%] left-0 fixed">
 			<ul>
-				<li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
+				{links.map((link) => (
+					<li
+						key={link.id}
+						className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${link.style}`}
+					>
+						<a
+							href={link.href}
+							className="flex justify-between items-center w-full text-white"
+							download={link.download}
+						>
+							{link.child}
+						</a>
+					</li>
+				))}
+				{/* <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500">
 					<a
 						href="https://www.linkedin.com/in/antony-poure-874297232/"
 						className="flex justify-between items-center w-full text-white"
@@ -16,7 +72,7 @@ const SocialLinks = () => {
 							LinkedIn <FaLinkedin size={30} />
 						</>
 					</a>
-				</li>
+				</li> */}
 			</ul>
 		</div>
 	);
