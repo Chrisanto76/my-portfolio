f0e54faf49f34faffbf8e0b7ab65b30e100b2837
Chrisanto76 <antonypoure2@gmail.com>
2024-01-21 18:12:04 +0100

    Add Portfolio component and update About component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import SocialLinks from './components/SocialLinks';
 import About from './components/About';
+import Portfolio from './components/Portfolio';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Navbar />
 			<Home />
 			<About />
+			<Portfolio />
 			<SocialLinks />
 		</div>
 	);
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,16 @@ const About = () => {
 			name="about"
 			className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
 		>
-			<div>
-				<div>
-					<p>About</p>
+			<div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+				<div className="pb-8">
+					<p
+						className="text-4xl font-bold inline border-b-4 bg
+                    border-gray-500"
+					>
+						About
+					</p>
 				</div>
-				<p>
+				<p className="text-xl mt-20">
 					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
 					ipsam, rerum ratione doloribus molestiae veniam quia expedita adipisci
 					voluptates ullam et officia voluptas incidunt libero ducimus id
@@ -21,7 +26,7 @@ const About = () => {
 
 				<br />
 
-				<p>
+				<p className="text-xl">
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsa
 					aspernatur eius quas distinctio nobis deleniti doloremque facilis rem,
 					non sunt. Veniam sit culpa natus dignissimos doloribus voluptate ipsum
