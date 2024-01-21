6ca2b7b7832a3b1975eaac71108af17af553a284
Chrisanto76 <antonypoure2@gmail.com>
2024-01-21 15:07:40 +0100

    Add About component and update image styling in Home component

---
+++ src/components/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div
+			name="about"
+			className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+		>
+			<div>
+				<div>
+					<p>About</p>
+				</div>
+				<p>
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+					ipsam, rerum ratione doloribus molestiae veniam quia expedita adipisci
+					voluptates ullam et officia voluptas incidunt libero ducimus id
+					excepturi hic mollitia sunt soluta obcaecati iusto! Nulla fugit
+					distinctio architecto nesciunt? Consequatur assumenda reprehenderit
+					perferendis in. Incidunt commodi eaque quae voluptates placeat.
+				</p>
+
+				<br />
+
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque ipsa
+					aspernatur eius quas distinctio nobis deleniti doloremque facilis rem,
+					non sunt. Veniam sit culpa natus dignissimos doloribus voluptate ipsum
+					voluptates aliquam fugiat harum! Provident est omnis dolorem eos
+					deserunt quia doloremque, itaque doloribus odit distinctio culpa, rem
+					ipsum quod error.
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default About;
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -37,7 +37,7 @@ const SocialLinks = () => {
 			id: 4,
 			child: (
 				<>
-					Resume <BsFillPersonLinesFill size={30} />
+					CV <BsFillPersonLinesFill size={30} />
 				</>
 			),
 			href: './resume.pdf',
@@ -47,21 +47,20 @@ const SocialLinks = () => {
 	];
 
 	return (
-		<div className="flex flex-col top-[35%] left-0 fixed">
+		<div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
 			<ul>
 				{links.map(({ id, child, href, style, download }) => (
 					<li
 						key={id}
-						className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500"
+						className={
+							'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' +
+							' ' +
+							style
+						}
 					>
 						<a
 							href={href}
-							className={
-								// eslint-disable-next-line no-useless-concat
-								'flex justify-between items-center w-full text-white' +
-								' ' +
-								style
-							}
+							className="flex justify-between items-center w-full text-white" // eslint-disable-next-line no-useless-concat
 							download={download}
 							target="_blank"
 							rel="noreferrer"
