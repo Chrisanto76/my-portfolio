4384e3bde7899ff6d2a065ae1ac741539c695291
Chrisanto76 <antonypoure2@gmail.com>
2024-03-19 16:41:49 +0100

    Add new technologies to Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,6 +7,44 @@ import node from '../assets/node.png';
 import tailwind from '../assets/tailwind.png';
 
 const Experience = () => {
+	const techs = [
+		{
+			id: 1,
+			src: html,
+			title: 'HTML',
+			style: 'shadow-orange-500',
+		},
+		{
+			id: 2,
+			src: css,
+			title: 'CSS',
+			style: 'shadow-blue-500',
+		},
+		{
+			id: 3,
+			src: javascript,
+			title: 'JAVA SCRIPT',
+			style: 'shadow-yellow-500',
+		},
+		{
+			id: 4,
+			src: react,
+			title: 'REACT',
+			style: 'shadow-blue-600',
+		},
+		{
+			id: 5,
+			src: node,
+			title: 'NODE',
+			style: 'shadow-green-500',
+		},
+		{
+			id: 1,
+			src: tailwind,
+			title: 'TAILWIND CSS',
+			style: 'shadow-sky-400',
+		},
+	];
 	return (
 		<div
 			name="experience"
@@ -14,15 +52,22 @@ const Experience = () => {
 		>
 			<div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
 				<div>
-					<p>Experience</p>
-					<p>these are technologies I've worked with</p>
+					<p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline ">
+						Experience
+					</p>
+					<p className="py-6">these are technologies I've worked with</p>
 				</div>
 
-				<div>
-					<div>
-						<img src={html} alt="" />
-						<p>HTML</p>
-					</div>
+				<div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+					{techs.map(({ id, src, title, style }) => (
+						<div
+							key={id}
+							className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+						>
+							<img src={src} alt="" className="w-20 mx-auto" />
+							<p className="mt-4">{title}</p>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
